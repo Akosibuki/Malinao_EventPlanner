59e556cfc07278834d5f3f8c4725bfeb3897a978
Akosibuki <malinaojoel843@gmail.com>
2025-07-08 10:21:46 +0800

    Add existing project files to Git

---
+++ MyFirstReactApp/src/components/CardComponent.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  const handleLike = () => setLikes(likes + 1);
+  const handleDislike = () => setDislikes(dislikes + 1);
+
+  return (
+    <div className="container">
+      <h2>
+        Charles Francis Padua, BSIT 3 of CCDI Sorsogon wins Gold Medal in IT Software Solutions
+        for Business category of the recent Philippine National Skills Competition at World Skills
+        Philippines held last August 21-25, 2024 at the World Trade Center
+      </h2>
+
+      <div className="buttons">
+        <button onClick={handleLike}>Like ({likes})</button>
+        <button onClick={handleDislike}>Dislike ({dislikes})</button>
+      </div>
+
+      <p>Total Ratings: {likes + dislikes}</p>
+    </div>
+  );
+}
+
+export default App;
